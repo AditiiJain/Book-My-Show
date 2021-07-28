e7ec1332646f0b872e2439e53c2966f3b1abfff1
AditiiJain <iaditi.jain08@gmail.com>
2021-07-28 20:33:18 +0530

    navbar completed

---
+++ src/components/HeroCarousel/HeroCarousel.component.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import HeroSlider from "react-slick";
+
+// Import css files
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const HeroCarousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  const images = [
+    "https://images.unsplash.com/photo-1627366285897-f040e8432f0f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    "https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1627307770076-fc575f0a9dc9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+    "https://images.unsplash.com/photo-1627465952353-89bca14c390e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
+    "https://images.unsplash.com/photo-1622495893726-f1478dcebc03?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=819&q=80",
+  ];
+  return (
+    <>
+      <HeroSlider {...settings}>{images.map((img) => {
+          <div className="w-20 h-80">
+              <img src={img} alt="carousel image" className="w-full h-full"/>
+          </div>
+      })}</HeroSlider>
+    </>
+  );
+};
+
+export default HeroCarousel;
