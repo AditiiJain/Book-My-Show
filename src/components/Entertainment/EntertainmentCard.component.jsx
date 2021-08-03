bd56d4ca1fae4acbf393a0e4d9b35d7793fce281
AditiiJain <iaditi.jain08@gmail.com>
2021-08-03 17:47:54 +0530

    adding multi row  entertainment carousel in small screens

--- src/components/Entertainment/EntertainmentCard.component.jsx
+++ src/components/Entertainment/EntertainmentCard.component.jsx
@@ -8,10 +8,10 @@ import "slick-carousel/slick/slick-theme.css";
 const EntertainmentCard = (props) => {
   return (
     <>
-      <div className="w-full h-30 px-4 md:h-46 sm:h-50 sm:px-2">
+      <div className="w-full h-30 md:px-4 md:h-46  px-2 py-2 md:py-0">
         <img
           src={props.src}
-          className="w-full h-full rounded-xl"
+          className="w-full h-full md:rounded-xl rounded-md"
           alt="entertainment image"
         />
       </div>
@@ -27,14 +27,25 @@ const EntertainmentCardSlider = () => {
 
     "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-NTUrIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/comedy-shows-collection-202007220710.png",
     "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MjUrIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/self-improvement-collection-202007220710.png",
-    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MyBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/music-shows-collection-202007220710.png",
+    "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MyBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/music-shows-collection-202007220710.png","https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MzArIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/art-and-crafts-collection-202007220710.png",
     "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MTUrIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/esports-collection-202011150107.png",
     "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-NSBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/food-n-drinks-collection-202010061250.png",
     "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-NCBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/interactive-games-collection-202012041129.png",
     "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-MTArIEV2ZW50cw%3D%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/theatre-shows-collection-202012041128.png",
     "https://in.bmscdn.com/discovery-catalog/collections/tr:w-800,h-800:ote-NCBFdmVudHM%3D,otc-FFFFFF,otf-Roboto,ots-64,ox-48,oy-320,ott-b:w-300/adventure-collection-202010140844.png",
   ];
-  const settings = {
+
+  const settingsSM = {
+    className: "center",
+    centerMode: true,
+    centerPadding: "60px",
+    slidesToShow: 2,
+    speed: 500,
+    rows: 1,
+    slidesPerRow: 2,
+  };
+
+  const settingsLG = {
     infinite: false,
     autoplay: false,
     slidesToShow: 5,
@@ -58,19 +69,26 @@ const EntertainmentCardSlider = () => {
           initialSlide: 1,
         },
       },
-      {
-        breakpoints: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-        },
-      },
+      // {
+      //   breakpoints: 639,
+      //   settings: {
+      //     slidesToShow: 2,
+      //     slidesToScroll: 1,
+      //   },
+      // },
     ],
   };
   return (
     <>
-      <div className="lg:container lg:mx-auto lg:px-4 md:px-2">
-        <Slider {...settings}>
+      <div className="hidden md:block lg:container lg:mx-auto lg:px-4 md:px-2">
+        <Slider {...settingsLG}>
+          {entertainmentImage.map((img) => {
+            return <EntertainmentCard src={img} />;
+          })}
+        </Slider>
+      </div>
+      <div className="md:hidden">
+        <Slider {...settingsSM}>
           {entertainmentImage.map((img) => {
             return <EntertainmentCard src={img} />;
           })}
--- src/pages/Home.pages.jsx
+++ src/pages/Home.pages.jsx
@@ -6,10 +6,11 @@ import EntertainmentCardSlider from "../components/Entertainment/EntertainmentCa
 const HomePage = () => {
   return (
     <>
-      <div className="lg:container lg:mx-auto lg:px-4">
-        <h2 className="font-bold text-gray-800 text-2xl p-6">
-          The Best of Entertainment{" "}
+      <div className="container mx-auto px-3 md:px-0 lg:px-4">
+        <h2 className="font-bold text-gray-800 md:text-2xl md:p-6 text-xl pt-4">
+          The Best Of Entertainment{" "}
         </h2>
+        <p className="text-gray-500 text-sm pb-4 md:hidden">Step out or stay in, interesting experiences for everyone.</p>
         <EntertainmentCardSlider />
       </div>
     </>
