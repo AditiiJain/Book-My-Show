72b2b1f0b706d10c7696605a2e9f5c019d9e08d3
AditiiJain <iaditi.jain08@gmail.com>
2021-08-06 22:09:11 +0530

    adding movie carousel

---
+++ src/components/MovieHero/MovieHero.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const MovieHero = () => {
+    return (<>
+    <div>
+        <div className="md:hidden">
+            <img src="" alt="" />
+        </div>
+        <div className="hidden md:block lg:hidden"></div>
+        <div className="hidden lg:block"></div>
+    </div>
+    </>  );
+}
+ 
+export default MovieHero;
