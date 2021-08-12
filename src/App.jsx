9c1e7de9ee4d7aeb875349899d35d62045d0f24e
AditiiJain <iaditi.jain08@gmail.com>
2021-08-12 21:58:33 +0530

    add TMDB API

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import Axios from "axios"
 
 //HOC
 import DefaultHOC from "./HOC/Default.HOC"
@@ -9,6 +10,13 @@ import PlaysHOC from "./HOC/Plays.HOC"
 import HomePage from "./pages/Home.pages";
 import MoviePage from "./pages/Movie.page";
 import PlaysPage from "./pages/Plays.pages";
+import axios from "axios";
+
+// axios default settings
+axios.defaults.baseURL="https://api.themoviedb.org/3";
+axios.defaults.params = {}; 
+//these are default settings and initially params are not existed. Therefore, it will throw an error that params not exist. That's why we are initializing an empty object and assigning API_KEY as the property of that object
+axios.defaults.params["api_key"] = process.env.REACT_APP_API_KEY;
 
 function App() {
   return (
--- src/components/HeroCarousel/HeroCarousel.component.jsx
+++ src/components/HeroCarousel/HeroCarousel.component.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import HeroSlider from "react-slick";
-import {NextArrow,PrevArrow} from "./Arrows.components";
 
+//components
+import { NextArrow, PrevArrow } from "./Arrows.components";
 
 // Import css files
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./HeroCarousel.css"
-
+import "./HeroCarousel.css";
 
 const HeroCarousel = () => {
+  //all hooks should be on top
+  const [images, setImages] = useState([]);
+
+  //naive approach-->here if we make the main callback function as async function, it will break the flow of react app. This will leads to RACE CONDITION.
+  // useEffect(async()=>{
+  // const getImages=await axios.get("/movie/now_playing");
+  // console.log(getImages);
+  // },[])
+
+  useEffect(() => {
+    const requestNowPLayingImages = async () => {
+      const getImages = await axios.get("/movie/now_playing");
+      // console.log(getImages)
+      setImages(getImages.data.results);
+    };
+    requestNowPLayingImages();
+  }, []);
+
+  const carouselImages = images.slice(0, 5);
   const settingsLG = {
     arrows: true,
-    autoplay:true,
+    autoplay: true,
     centerMode: true,
     centerPadding: "338px",
     dots: true,
@@ -21,46 +41,48 @@ const HeroCarousel = () => {
     slidesToShow: 1,
     slidesToScroll: 1,
     // own custom arrows for carousel
-    nextArrow:<NextArrow/>,
-    prevArrow:<PrevArrow/>
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
   const settings = {
     arrows: true,
     dots: true,
     infinite: true,
-    autoplay:true,
+    autoplay: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    nextArrow:<NextArrow/>,
-    prevArrow:<PrevArrow/>
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
-  const images = [
-    "https://images.unsplash.com/photo-1627366285897-f040e8432f0f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-    "https://images.unsplash.com/photo-1606787366850-de6330128bfc?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-    "https://images.unsplash.com/photo-1627307770076-fc575f0a9dc9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
-    "https://images.unsplash.com/photo-1627465952353-89bca14c390e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=334&q=80",
-    "https://images.unsplash.com/photo-1622495893726-f1478dcebc03?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=819&q=80",
-  ];
+
   return (
     <>
       <div className="lg:hidden">
         <HeroSlider className=" heroCarousel" {...settings}>
-          {images.map((image) => {
+          {carouselImages.map((image) => {
             return (
               <div className="w-full h-56 md:h-72 sm:h-64 pb-3">
-                <img src={image} alt="" className="w-full h-full" />
+                <img
+                  src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
+                  alt="carousel"
+                  className="w-full h-full"
+                />
               </div>
             );
           })}
         </HeroSlider>
       </div>
       <div className="hidden lg:block">
-        <HeroSlider   className=" heroCarousel" {...settingsLG}>
-          {images.map((image) => {
+        <HeroSlider className=" heroCarousel" {...settingsLG}>
+          {carouselImages.map((image) => {
             return (
-              <div className="w-full h-96 px-2 py-3">             
-                <img src={image} alt="" className="w-full h-full rounded-md" />
+              <div className="w-full h-96 px-2 py-3">
+                <img
+                  src={`https://image.tmdb.org/t/p/original${image.backdrop_path}`}
+                  alt=""
+                  className="w-full h-full rounded-md"
+                />
               </div>
             );
           })}
